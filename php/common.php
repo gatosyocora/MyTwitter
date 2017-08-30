e5a0b6cbc7d1cb81b093f5d907d54a668f591abd
gatosyocora <kut190316q@gmail.com>
2017-08-30 15:20:15 +0900

    Delete common.php

--- php/common.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $page = "http://dev.twitter.local";
-
-  define( 'CONSUMER_KEY', 'PuOy72akuCC8hpRGWSj0u14Nm' );
-  define( 'CONSUMER_SECRET', 'iSh4HwAC6TRkCM90YOvQQMWb0DG2BK4XzuLhFSYtH1ZxwLGnK1' );
-  define( 'OAUTH_CALLBACK', $page. '/php/callback.php' );
- ?>
