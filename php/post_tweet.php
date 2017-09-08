4541e56cf1034270c7ac7574143bc5d7b19e8695
gatosyocora <kut190316q@gmail.com>
2017-09-08 11:27:25 +0900

    post_tweet.phpを作成

--- php/post_tweet.php
+++ php/post_tweet.php
@@ -4,22 +4,28 @@
 ****************************************************************************/
 
   // OAuthライブラリの読み込み
-  require "twitteroauth/autoload.php";
+  require_once "twitteroauth/autoload.php";
+  require_once "common.php";
   use Abraham\TwitterOAuth\TwitterOAuth;
 
+  session_start();
+
   // @gatosyocora
-  $api_key = "PuOy72akuCC8hpRGWSj0u14Nm"; // APIキー
-  $api_secret = "iSh4HwAC6TRkCM90YOvQQMWb0DG2BK4XzuLhFSYtH1ZxwLGnK1"; // APIシークレット
-  $access_token = "886632174400659456-CtWSWoHuBkYaW00HfTmROTKIhRICPTE"; // アクセストークン
-  $access_token_secret = "OAaOHYW5g7prJFGtENLGJZD7oAJRcDOXfhmstxTTuU160"; // アクセストークンシークレット
+  $access_token = [];
+  $access_token = $_SESSION['access_token'];
 
   // 接続
-  $connection = new TwitterOAuth($api_key, $api_secret, $access_token, $access_token_secret);
+  $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
 
-  // ツイート
-  $res = $connection->post("statuses/update", array("status" => "hello"));
+  $tweettext = $_POST["tweetdata"];
+  if (count($tweettext) > 0) {
+    // ツイート
+    $res = $connection->post("statuses/update", array("status" => $tweettext));
+    //レスポンス確認
+    echo str_replace("'", "’", json_encode($res));
+  } else {
+    echo "ツイートデータの受け取りに失敗しました";
+  }
 
-  //レスポンス確認
-  var_dump($res);
 
  ?>
