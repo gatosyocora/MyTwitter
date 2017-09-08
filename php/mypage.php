71a9f341b574f91eee6543445aa33e57e88b5b35
gatosyocora <kut190316q@gmail.com>
2017-09-08 11:32:52 +0900

    表示する情報を変更

--- php/mypage.php
+++ php/mypage.php
@@ -18,14 +18,24 @@
   $user = $connection->get("account/verify_credentials");
 
   //取得したユーザー情報を構造化された情報へ変換(ダンプ)し, 表示
-  var_dump( $user );
+  //var_dump( $user );
 
+  // ユーザーのタイムラインを10件取得
+  $timeline = $connection->get("statuses/home_timeline", array("count"=>10));
+
+  //var_dump( $timeline );
+
+  /*** 情報をHTML形式で表示 */
   header("Content-type: text/html; charset=utf-8");
 
   // ユーザー情報を表示
   echo "<h2>あなたの名前: ". $user->name ."</h2>";
   echo "<h3>あなたのユーザーID: ". $user->screen_name ."</h3>";
-  echo "<h3>最終ツイート: " . $user->status->text . "</h3>";
-  echo "<h4>" . date("Y年n月j日l H:i:s", strtotime($user->status->created_at)) . "</h4>";
+  echo "<p><h3>最終ツイート: " . $user->status->text . "</h3><br>". date("Y年n月j日l H:i:s", strtotime($user->status->created_at)) . "</p>";
+  echo "<ul>";
+  foreach ($timeline as $key => $value) {
+    echo "<li><b><u>" . $value->user->name ."[@" . $value->user->screen_name ."]</u></b>: <br>" . $value->text . "</li>";
+  };
+  echo "</ul>";
   echo "<a href='logout.php'>ログアウト</a>";
  ?>
